acbbd1ec4c68ef862b2f528205ed911de869e843
zenor0 <zenor0@qq.com>
2023-07-06 22:20:48 +0800

    fix: adapt to Java-side design

--- app/api/v2/orders/views.py
+++ app/api/v2/orders/views.py
@@ -7,7 +7,7 @@ from sqlalchemy.sql import text
 from ..models import BaseResponse
 from flask_jwt_extended import jwt_required
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from flask_restful import Resource, Api
 
@@ -25,8 +25,10 @@ class OrdersList(Resource):
             filter_condition.add(Order.to_id == request.args.get('to_id'))
         if request.args.get('good_id'):
             filter_condition.add(Order.good_id == request.args.get('good_id'))
-        if request.args.get('state') in Order.ORDER_STATE_ENUM:
-            filter_condition.add(Order.state == request.args.get('state'))
+        if request.args.get('state', '', type=str).lower() in Order.ORDER_STATE_ENUM_DESCRIPTION:
+            state = Order.ORDER_STATE_ENUM_DESCRIPTION.index(request.args.get('state', '', type=str).lower())
+            print(state)
+            filter_condition.add(Order.state == Order.ORDER_STATE_ENUM[state])
         
         order_by = request.args.get('order_by', '', type=str)
         order_list = ['asc', 'desc']
@@ -45,9 +47,10 @@ orderApi.add_resource(OrdersList, '/')
 def get_order_stats():
     this_week = datetime.now().isocalendar()[1]
     this_month = datetime.now().month
+    time_now = datetime.now()
     
-    order_list_this_month = Order.query.filter(and_(Order.state == Order.ORDER_STATE_ENUM[2], extract('month', Order.create_time) == this_month)).all()
-    order_list_last_month = Order.query.filter(and_(Order.state == Order.ORDER_STATE_ENUM[2], extract('month', Order.create_time) == this_month-1%12)).all()
+    order_list_this_month = Order.query.filter(and_(Order.state == Order.ORDER_STATE_ENUM[2], time_now - timedelta(days=30) <= Order.pay_time)).all()
+    order_list_last_month = Order.query.filter(and_(Order.state == Order.ORDER_STATE_ENUM[2], time_now - timedelta(days=60) <= Order.pay_time, Order.pay_time <= time_now - timedelta(days=30))).all()
     
     this_month_amount_array = []
     for i in range(1, 30):
